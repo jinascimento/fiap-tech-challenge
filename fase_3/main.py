4c2e231bed11d3426717e4fb39f02989d6dbaa26
Rodrigo Braga <rbraga@gmail.com>
2026-02-23 22:13:05 -0300

    feat: create a main file
    creates a file to coordinate execution flow from project

---
+++ fase_3/main.py
@@ -0,0 +1,24 @@
+import logging
+import os
+
+from config.settings import settings
+from pre_processing.pipeline import run as pipeline
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler(settings.LOG_FILE),
+        logging.StreamHandler(),
+    ],
+)
+
+logger = logging.getLogger("fase_3")
+
+os.environ["HF_TOKEN"] = settings.HF_TOKEN
+
+
+if __name__ == "__main__":
+    logger.info("Executando o pipeline para carregamento dos dados para treinamento")
+
+    pipeline()
